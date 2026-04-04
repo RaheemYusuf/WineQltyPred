40002c22b9a8292c62ba836a12efad535f41f4bc
Yusuf Raheem <yusufraheem500@gmail.com>
2026-04-04 13:08:51 +0100

    Added Logging functionality

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.housepricepred import logger
+
+logger.info("Welcome to our custom logging for House Prediction")
--- template.py
+++ template.py
@@ -1,12 +1,11 @@
-import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
-project_name="housepricepred"
+project_name = "housepricepred"
 
-list_of_files=[
+list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -28,19 +27,19 @@ list_of_files=[
     "templates/index.html",
 ]
 
-for filepath in list_of_files:
-    filepath=Path(filepath)
-    filedir,filename=os.path.split(filepath)
-    
-    if filedir!="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory {filedir} for the file : {filename}")
-        
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
-            
-            pass
-            logging.info(f"Creating empty file: {filepath}")
-    
+for file_path in list_of_files:
+    filepath = Path(file_path)
+    filedir = filepath.parent
+
+    if filedir != Path('.'):
+        filedir.mkdir(parents=True, exist_ok=True)
+        logging.info(
+            f"Creating directory {filedir} for file: {filepath.name}"
+        )
+
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
     else:
-        logging.info(f"{filename} already exists!")
+        logging.info(f"{filepath.name} already exists!")
+
